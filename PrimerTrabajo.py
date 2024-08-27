b44d9a645d8af765f100f45166858f1a50dd94ee
Jiame008 <jaimearmenterosv@gmail.com>
2024-08-26 22:41:22 -0400

    Create PrimerTrabajo.py

---
+++ PrimerTrabajo.py
@@ -0,0 +1,5 @@
+# Sorprende a tus compañeros
+import random
+for i in range(5):
+    datos = ["Mi aficion favorita es la creacion de arte(cualquiera)", "Tengo dos gatos", "Yo se usar Blender, Unity y Fl studio", "No c usar python", "Venci Getting Over It with Bennett Foddy en 9 minutos 14 segundos"]
+    print(random.choice(datos))
